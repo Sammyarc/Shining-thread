cfacfca3594897cc6d38b41cb3b6243ad23fee27
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2023-10-23 01:12:09 +0100

    Added emailjs and fixed minor bugs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// MENU JAVASCRIPT
 const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
 
@@ -13,6 +14,8 @@ btn.addEventListener('click', () => {
         .toggle('hidden');
 })
 
+
+// NAVBAR BACKGROUND CHANGE
 document.addEventListener("DOMContentLoaded", function () {
     const navbar = document.getElementById("navbar");
 
@@ -29,3 +32,31 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// EMAIL JAVASCRIPT
+
+function sendMail() {
+    var params = {
+        firstName: document.getElementById("first_name").value,
+        email: document.getElementById("email_address").value,
+        phoneNumber: document.getElementById("phone_number").value,
+        contact: document.getElementById("contact").value,
+        message: document.getElementById("message").value,
+    };
+
+
+const serviceID = "service_5p5kaj9";
+const templateID = "template_gbsb0gk";
+
+emailjs.send(serviceID, templateID, params).then((res) => {
+        document.getElementById("first_name").value = "";
+        document.getElementById("email_address").value = "";
+        document.getElementById("phone_number").value = "";
+        document.getElementById("contact").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Message sent successfully");
+    }
+ )
+ .catch((err) => console.log(err));
+
+}
