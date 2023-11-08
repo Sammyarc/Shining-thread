e8bafefe5ccd2d13c9c98f6bfa3b303a915491b0
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2023-11-08 04:08:47 +0100

    Commits

--- js/script.js
+++ js/script.js
@@ -94,3 +94,19 @@ const callback = function (entries) {
     target.classList.add("opacity-0");
     observer.observe(target);
   });
+
+  // TEAM CAROUSEL
+
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
